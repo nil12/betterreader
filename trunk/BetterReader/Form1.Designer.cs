5bf9714c9eaae8d5c5172dc3b7b76d3d3288fbdc
stevekain <stevekain@d095f4f6-cc1c-0410-8f42-1f0e3bb3b119>
2006-09-11 02:14:01 +0000

    Update Timers implemented.

--- trunk/BetterReader/Form1.Designer.cs
+++ trunk/BetterReader/Form1.Designer.cs
@@ -274,6 +274,7 @@ namespace BetterReader
 			this.MainMenuStrip = this.mainMenuStrip;
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.mainMenuStrip.ResumeLayout(false);
 			this.mainMenuStrip.PerformLayout();
